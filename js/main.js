d5b37b663602a0f32dbc7040ea5bbdd8d7b371bc
Supatchaya <tai.pakdee@gmail.com>
2021-04-11 00:26:38 -0600

    Added import for player class, added global variables for yards, par, and handicap, added logic to use player class, finished dynamic card building, and added logic to finish requirements.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,20 @@
+import Player from './player.js';
+
 let course;
 let numHoles;
 let courseId = 0;
 const players = [];
-const outScores = [];
-const inScores = [];
+let yardIn = 0;
+let yardOut = 0;
+let parIn = 0;
+let parOut = 0;
+let hcpIn = 0;
+let hcpOut = 0;
 
-document
-    .getElementById('select-course')
-    .addEventListener('change', (event) => {
-        courseId = event.target.value;
-        chooseCourse(courseId);
-    });
-
-document
-    .getElementById('select-tee')
-    .addEventListener('change', (event) => {
-        createCard(event.target.value);
-    });
-
-function getCourses() {
+const getCourses = () => {
     $.get('https://golf-courses-api.herokuapp.com/courses', (data) => {
         data['courses'].forEach(
-            (course, index) => {
+            (course) => {
                 $('#select-course').append(`<option value="${course.id}">${course.name}</option>`)
             } 
         )
@@ -37,7 +30,6 @@ function chooseCourse(courseId) {
         course.holes[0].teeBoxes.forEach(
             (teeBox, index) => {
                 $('#select-tee').append(`<option value="${index}">${teeBox.teeType}</option>`)
-            
             }
         )
         createCard(0);
@@ -62,16 +54,24 @@ function createCard(teeType) {
             $("#par").append(`<div class = "parBox" id = "par-${holeInfo[hole].hole}"<span>${teeBox.par}</span></div>`);
             $("#handicap").append(`<div class = "hcpBox" id = "hcp-${holeInfo[hole].hole}"<span>$${teeBox.hcp}</span></div>`);   
         }
-    }
-    else {
+    } else {
         for (let hole = 0; hole <= 8; hole++) { 
             let teeBox = holeInfo[hole].teeBoxes[teeType];
             $("#holes").append(`<th scope="col" class="holeBox">${holeInfo[hole].hole}</th>`);
             $("#yardage").append(`<td class="yardBox">${teeBox.yards}</td>`);
             $("#par").append(`<td class="parBox">${teeBox.par}</td>`);
             $("#handicap").append(`<td class="hcpBox">${teeBox.hcp}</td>`);
-
+            yardOut += teeBox.yards;
+            parOut += teeBox.par;
+            hcpOut += teeBox.hcp;
         }
+
+         $('#holes').append(`<th scope="col" class="holeBox">OUT</th>`);
+         $("#yardage").append(`<td class="yardBox">${yardOut}</td>`);
+         $("#par").append(`<td class="parBox">${parOut}</td>`);
+         $("#handicap").append(`<td class="hcpBox">${hcpOut}</td>`);
+
+         
 
         for (let hole = 9; hole <= 17; hole++) {
             let teeBox = holeInfo[hole].teeBoxes[teeType];
@@ -79,75 +79,182 @@ function createCard(teeType) {
             $("#yardage").append(`<td class="yardBox">${teeBox.yards}</td>`);
             $("#par").append(`<td class="parBox">${teeBox.par}</td>`);
             $("#handicap").append(`<td class="hcpBox">${teeBox.hcp}</td>`);
+            yardIn += teeBox.yards;
+            parIn += teeBox.par;
+            hcpIn += teeBox.hcp;
 
 
         }
 
+        $('#holes').append(`<th scope="col" class="holeBox">IN</th>`);
+        $("#yardage").append(`<td class="yardBox">${yardIn}</td>`);
+        $("#par").append(`<td class="parBox">${parIn}</td>`);
+        $("#handicap").append(`<td class="hcpBox">${hcpIn}</td>`);
+       
+
+         
     }
+        $('#holes').append(`<th scope="col" class="holeBox">TOTAL</th>`);
+        $("#yardage").append(`<td class="yardBox">${yardIn + yardOut}</td>`);
+        $("#par").append(`<td class="parBox">${parIn + parOut}</td>`);
+        $("#handicap").append(`<td class="hcpBox">${hcpIn + hcpOut}</td>`);
 }
 
-function addPlayer(name) {
-    const namingError = 'This name is taken.';
-    const numPlyrErr = 'There are already four players.';
-    if (!players.includes(name)) {
-        if (players.length !== 4) {
+const addPlayer = () => {
+    let name = '';
+    if ($('#name-input').val()) {
+        name = $('#name-input').val();
+        const namingError = 'This name is taken. Please choose a new name';
+        const numPlyrErr = 'There are already four players.';
+        const newPlayer = new Player(name);
+        if (players.length) {
+            players.forEach(
+                (_player) => {
+                    if (_player.name.toLowerCase() === newPlayer.name.toLowerCase()) {
+                        $('#error-msg').html(namingError);
+                        setTimeout(() => {
+                            $('#error-msg').html('');
+                        }, 2000);
+                        $('#name-input').val('');
+                        return false;
+                    }
+                }
+            );
+            if (players.length === 4) {
+                $('#error-msg').html(numPlyrErr);
+                setTimeout(() => {
+                    $('#error-msg').html('');
+                }, 2000);
+                $('#name-input').val('');
+                return false;
+            }
             const playerInfo = `
-                <tr id="${name}">
-                    <th scope="row">${name}</th>
+                        <tr id="${newPlayer.name}">
+                            <th scope="row">${newPlayer.name}</th>
+                        </tr>`;
+            $('#scorecard').append(playerInfo);
+            addPlayerInfo(newPlayer);
+            players.push(newPlayer);
+        } else {
+            const playerInfo = `
+                <tr id="${newPlayer.name}">
+                    <th scope="row">${newPlayer.name}</th>
                 </tr>`;
             $('#scorecard').append(playerInfo);
-            addPlayerInfo(name);
-            players.push(name);
-        } else {
-            $('#error-msg').html(numPlyrErr);
-            setTimeout(() => {
-                $('#error-msg').html('');
-            }, 2000);
+            addPlayerInfo(newPlayer);
+            players.push(newPlayer);
         }
-    } else {
-        $('#error-msg').html(namingError);
-            setTimeout(() => {
-                $('#error-msg').html('');
-            }, 2000);
+        $('#name-input').val('');
     }
-    $('#name-input').val('');
 }
 
-function addPlayerInfo(name) {
-    const tableData = `
-        <td>
-            <input class="table-data" type="number"  min="-9" max="9" onfocusout="lockScore(this)" owner="${name}"/>
-        </td>`;
+function addPlayerInfo(player) {
     if (numHoles < 18) {
         for (let hole = 0; hole <= 8; hole++) {
-            if (hole > 8) {
-                $(`#${name}`).append(tableData);
-            } else {
-                $(`#${name}`).append(tableDataLast);
-            }
+            const tableData = `
+                <td>
+                    <input id="${player.name}-input-${hole + 1}" class="table-data score-input" type="number"  min="-9" max="9" owner="${player.name}"/>
+                </td>`;
+            $(`#${player.name}`).append(tableData);
+            $(`#${player.name}-input-${hole + 1}`).focusout(lockScore);
         }
-        $(`#${name}`).append('<div id="out-total-score" class="table-data"><!-- out total score goes here --></div>');
-        $(`#${name}`).append('<div id="total-score" class="table-data"><!-- total score goes here --></div>');
+        $(`#${player.name}`).append(`<td id="${player.name}-out-total-score" class="table-data"><!-- out total score goes here --></td>`);
+        $(`#${player.name}`).append(`<td id="${player.name}-total-score" class="table-data"><!-- total score goes here --></td>`);
     } else {
         for (let hole = 0; hole <= 8; hole++) { 
-            $(`#${name}`).append(tableData);
+            const tableData = `
+                <td>
+                    <input id="${player.name}-input-${hole + 1}" class="table-data score-input" type="number"  min="-9" max="9" owner="${player.name}"/>
+                </td>`;
+            $(`#${player.name}`).append(tableData);
+            $(`#${player.name}-input-${hole + 1}`).focusout(lockScore);
         }
-        $(`#${name}`).append('<div id="out-total-score" class="table-data"><!-- out total score goes here --></div>');
+        $(`#${player.name}`).append(`<td id="${player.name}-out-total-score" class="table-data"><!-- out total score goes here --></td>`);
     
         for (let hole = 9; hole <= 17; hole++) {
-            $(`#${name}`).append(tableData);
+            const tableData = `
+                <td>
+                    <input id="${player.name}-input-${hole + 1}" class="table-data score-input" type="number"  min="-9" max="9" owner="${player.name}"/>
+                </td>`;
+            $(`#${player.name}`).append(tableData);
+            $(`#${player.name}-input-${hole + 1}`).focusout(lockScore);
         }
-        $(`#${name}`).append('<div id="in-total-score" class="table-data"><!-- in total score goes here --></div>');
-        $(`#${name}`).append('<div id="total-score" class="table-data"><!-- total score goes here --></div>');
+        $(`#${player.name}`).append(`<td id="${player.name}-in-total-score" class="table-data"><!-- in total score goes here --></td>`);
     }
+    $(`#${player.name}`).append(`<td id="${player.name}-total-score" class="table-data"><!-- total score goes here --></td>`);
 }
 
-function lockScore(target) {
+const lockScore = (event) => {
+    const target = event.target;
     if ($(target).val()) {
         $(target).attr('disabled', true);
         $('#error-msg').html(`Score locked for ${$(target).attr('owner')}.`);
         setTimeout(() => {
             $('#error-msg').html('');
         }, 2000);
+        const player = players.find(
+            (_player) => _player.name === $(target).attr('owner')
+        )
+        updateScore(player, $(target).val());
     }
 }
+
+function updateScore(player, score) {
+    if (numHoles < 18) {
+        if (player.outScores.length < 9) {
+            player.outScores.push(parseInt(score));
+            if (player.outScores.length === 9) {
+                $(`#${player.name}-total-score`).html(player.outScores.reduce(sumScores));
+                // getEndMsg()
+            }
+        }
+    } else {
+        if (player.outScores.length < 9) {
+            player.outScores.push(parseInt(score));
+            if (player.outScores.length === 9) {
+                $(`#${player.name}-out-total-score`).html(player.outScores.reduce(sumScores));
+            }
+        } else if (player.inScores.length < 9) {
+            player.inScores.push(parseInt(score));
+            if (player.inScores.length === 9) {
+                const totalScore = player.outScores.reduce(sumScores) + player.inScores.reduce(sumScores);
+                $(`#${player.name}-in-total-score`).html(player.outScores.reduce(sumScores));
+                $(`#${player.name}-total-score`).html(totalScore);
+                getEndMsg(player, totalScore);
+            }
+        }
+    }
+}
+
+const sumScores = (summedScores, newScore) => summedScores + newScore;
+    
+function getEndMsg(player, totalScore) {
+    const difference = Math.abs((parIn + parOut) - totalScore);
+    const positiveScoreMsg = `${difference} compared to par. On to the PGA, ${player.name}!`;
+    const negativeScoreMsg = `${difference} compared to par. Better luck next time, ${player.name}!`;
+    if (totalScore <= 108) {
+        $('.scoreCard-Container').append(`<span>${positiveScoreMsg}</span>`);
+    } else {
+        $('.scoreCard-Container').append(`<span>${negativeScoreMsg}</span>`);
+    }
+}
+
+$(document).ready(getCourses);
+
+document
+    .getElementById('select-course')
+    .addEventListener('change', (event) => {
+        courseId = event.target.value;
+        chooseCourse(courseId);
+    });
+
+document
+    .getElementById('select-tee')
+    .addEventListener('change', (event) => {
+        createCard(event.target.value);
+    });
+
+document
+    .getElementById('add-player')
+    .addEventListener('click', addPlayer);
+    
